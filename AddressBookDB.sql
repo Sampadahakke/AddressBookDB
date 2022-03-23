e06d836a3260e082cd3c3ae468dc138789e528cd
Sampada <sampadahakke102@gmail.com>
2022-03-23 13:25:50 +0530

    [Sampada] Creating addressbook table

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,2 +1,15 @@
+---------------------UC1-Creating Addressbook database--------------------
 Create Database AddressBookDB;
 Use AddressBookDB;
+----------------------UC2-Creating Addressbook table-----------------------
+Create Table AddressBook
+(
+  FirstName varchar(50) NOT NULL,
+  LastName varchar(50),
+  Address varchar(250),
+  City varchar(50),
+  State varchar(50),
+  Zip int,
+  PhoneNumber bigint,
+)
+
