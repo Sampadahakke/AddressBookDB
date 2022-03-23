9e4d415b6a51936f9cbf9cc3bf77af02def28756
Sampada <sampadahakke102@gmail.com>
2022-03-23 13:51:52 +0530

    [Sampada] Editing existing contacts

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -19,5 +19,8 @@ Insert into AddressBook (FirstName,LastName,Address,City,State,Zip,PhoneNumber) 
 ('Shubhada','Patil','Pune','Pune','Maharashtra',416308,9256342510),
 ('Venkatesh','Gavade','Kolhapur','Kolhapur','Maharashtra',416404,9256245320),
 ('Dhanshree','Hakke','Sangli','Sangli','Maharashtra',416416,8421729091)
-
+---------------------------UC4-Editing existing contacts------------------------------
+Update AddressBook set PhoneNumber=9765721521 where FirstName ='Venkatesh'
+Update AddressBook set LastName='Patil' where  FirstName='Dhanshree'
+Select * from AddressBook
 
