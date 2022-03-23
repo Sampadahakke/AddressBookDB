33c73b51718d9a209e75931494404ccc99040e15
Sampada <sampadahakke102@gmail.com>
2022-03-23 13:40:53 +0530

    [Sampada] Inserting new contacts

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -12,4 +12,12 @@ Create Table AddressBook
   Zip int,
   PhoneNumber bigint,
 )
+Select * from AddressBook
+--------------------UC3-Inserting new contacts in addressbook----------------------
+Insert into AddressBook (FirstName,LastName,Address,City,State,Zip,PhoneNumber) Values
+('Sampada','Hakke','Sangli','Sangli','Maharashtra',416416,8421729091),
+('Shubhada','Patil','Pune','Pune','Maharashtra',416308,9256342510),
+('Venkatesh','Gavade','Kolhapur','Kolhapur','Maharashtra',416404,9256245320),
+('Dhanshree','Hakke','Sangli','Sangli','Maharashtra',416416,8421729091)
+
 
